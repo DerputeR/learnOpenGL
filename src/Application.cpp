e3eaa7b7b81a64bd53f1ca3a869d803cdff23fd6
DerputeR <dekxy-developer@yahoo.com>
2023-10-31 11:46:31 -0400

    orange triangle

--- src/Application.cpp
+++ src/Application.cpp
@@ -7,10 +7,15 @@ const int kDefaultWindowHeight = 600;
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
+"void main() {\n"
 " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 "}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main() {\n"
+"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
+"}";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
@@ -20,6 +25,10 @@ void processInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
 	}
+}
+
+void DrawTriangle() {
+	/*glBindVertexArray(VAO);*/
 }
 
 int main() {
@@ -56,16 +65,70 @@ int main() {
 		0.0f, 0.5f, 0.0f
 	};
 
+
+
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	
+	// bind VAO
+	glBindVertexArray(VAO);
+	// copy vertices array to buffer
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	// ? should size be of entire array contents or just for the size of each element?
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// set vertex attributes pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
+	// compile vertex shader object
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
+
+	// check if compilation had errors
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << infoLog << std::endl;
+	}
+
+	// compile fragment shader object
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	// check if compilation had errors
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << infoLog << std::endl;
+	}
+
+	// link shader objects into shader program
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// check if linking had errors
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << infoLog << std::endl;
+	}
+
+	// delete individual shader objects after linking to program
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// activate shader program
+	glUseProgram(shaderProgram);
 
 	while (!glfwWindowShouldClose(window)) {
 		// input
@@ -74,6 +137,7 @@ int main() {
 		// render
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// check and call events and swap buffers
 		glfwPollEvents();
