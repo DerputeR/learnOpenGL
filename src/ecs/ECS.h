7ff8fe5374538c9d36ebce621287bc6cfaec41dc
DerputeR <dekxy-developer@yahoo.com>
2024-05-28 15:34:41 -0400

    Added skeleton template for iterator for SceneView (incomplete)

--- src/ecs/ECS.h
+++ src/ecs/ECS.h
@@ -1,11 +1,11 @@
 #pragma once
 #include <vector>
-#include <array>
 #include <bitset>
 #include <deque>
 #include <stdexcept>
 #include <string>
-#include <unordered_map>
+#include <iterator>
+#include <cstddef>
 
 namespace ECS
 {
@@ -309,5 +309,43 @@ namespace ECS
             ComponentPool<Component>* cpool = static_cast<ComponentPool<Component>*>(pool);
             cpool->unassign(entity.id);
         }
+    };
+
+    template <class... Component>
+    struct SceneView
+    {
+        SceneView(Scene& scene) : 
+
+        struct Iterator
+        {
+            using iterator_category = std::forward_iterator_tag;
+            using difference_type = std::ptrdiff_t;
+            using value_type = int;
+            using pointer = int*;  // or also value_type*
+            using reference = int&;  // or also value_type&
+
+            Iterator(pointer ptr) : m_ptr(ptr) {}
+
+            reference operator*() const { return *m_ptr; }
+            pointer operator->() { return m_ptr; }
+
+            // Prefix increment
+            Iterator& operator++() { m_ptr++; return *this; }
+
+            // Postfix increment
+            Iterator operator++(int) { Iterator tmp = *this; ++(*this); return tmp; }
+
+            friend bool operator== (const Iterator& a, const Iterator& b) { return a.m_ptr == b.m_ptr; };
+            friend bool operator!= (const Iterator& a, const Iterator& b) { return a.m_ptr != b.m_ptr; };
+
+        private:
+            pointer m_ptr;
+
+        };
+
+        Iterator begin() { return Iterator(&m_data[0]); }
+        Iterator end() { return Iterator(&m_data[200]); }
+
+        private
     };
 }
