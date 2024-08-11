7a318fb5d8b232dd59bccf45cac1da9be3b3ae95
DerputeR <dekxy-developer@yahoo.com>
2024-08-11 01:41:37 -0400

    Wrote pointer-based Transform component for ECS using quaternions for rotation

--- src/ecs/components/Transform.cpp
+++ src/ecs/components/Transform.cpp
@@ -1,6 +1,75 @@
 #include "Transform.h"
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
 
 namespace ECS::Components
 {
+    glm::vec3 Transform::getLocalPosition() const
+    {
+        return position;
+    }
 
+    glm::quat Transform::getLocalRotation() const
+    {
+        return rotation;
+    }
+
+    glm::vec3 Transform::getLocalScale() const
+    {
+        return scale;
+    }
+
+    glm::vec3 Transform::getPosition()
+    {
+        return glm::vec3{ parent->getMatrix() * glm::vec4{ position, 1.0f } };
+    }
+
+    glm::quat Transform::getRotation()
+    {
+        return glm::quat_cast(parent->getMatrix() * glm::mat4_cast(this->rotation));
+    }
+
+    glm::mat4 Transform::getMatrix()
+    {
+        if (dirty)
+        {
+            // translation * rotation * scale
+            // TODO: 
+            glm::mat4 translation = glm::translate(glm::mat4{ 1.0f }, this->position);
+            glm::mat4 rotation = glm::mat4_cast(this->rotation);
+            glm::mat4 scale = glm::scale(glm::mat4{ 1.0f }, this->scale);
+            transformationMatrix = translation * rotation * scale;
+        }
+        return transformationMatrix;
+    }
+
+    void Transform::setLocalPosition(const glm::vec3& position)
+    {
+        dirty = true;
+        this->position = position;
+    }
+
+    void Transform::setLocalScale(const glm::vec3& scale)
+    {
+        dirty = true;
+        this->scale = scale;
+    }
+
+    void Transform::setLocalRotation(const glm::quat& rotation)
+    {
+        dirty = true;
+        this->rotation = rotation;
+    }
+
+    void Transform::setLocalRotation(float angle, const glm::vec3& axis)
+    {
+        dirty = true;
+        this->rotation = glm::angleAxis(angle, axis);
+    }
+
+    void Transform::setLocalRotation(float pitch, float yaw, float roll)
+    {
+        dirty = true;
+        this->rotation = glm::quat(glm::vec3{ pitch, yaw, roll });
+    }
 }
--- src/ecs/components/Transform.h
+++ src/ecs/components/Transform.h
@@ -5,23 +5,93 @@
 
 namespace ECS::Components
 {
-    struct Transform
+    class Transform
     {
-        using transform_id = unsigned int;
-        const static transform_id NULL_TRANSFORM = -1;
+    public:
+        /**
+         * @return A copy of this Transform's local position vector
+         */
+        glm::vec3 getLocalPosition() const;
 
-        transform_id parent_id;
+        /**
+         * @return A copy of this Transform's local rotation quaternion
+         */
+        glm::quat getLocalRotation() const;
+
+        /**
+         * @return A copy of this Transform's local scale vector
+         */
+        glm::vec3 getLocalScale() const;
+
+        /**
+         * @brief Copies the local position with the parent's transformation matrix pre-multiplied to it, if one exists.
+         * @return A copy of this Transform's global position vector
+         */
+        glm::vec3 getPosition();
+
+        /**
+         * @brief Copies the local rotation with the parent's transformation matrix pre-multiplied to it, if one exists.
+         * @return A copy of this Transform's global rotation quaternion
+         */
+        glm::quat getRotation();
+
+        /**
+         * @brief Combines this Transform's position, rotation, and scale into one 4x4 matrix.
+         * @return A copy of this Transform's transformation matrix
+         */
+        glm::mat4 getMatrix();
+
+        /**
+         * @brief Updates this Transform's local position vector
+         * @param position 
+         */
+        void setLocalPosition(const glm::vec3& position);
+
+        /**
+         * @brief Updates this Transform's local scale vector
+         * @param scale
+         */
+        void setLocalScale(const glm::vec3& scale);
+
+        /**
+         * @brief Updates this Transform's local rotation quaternion
+         * @param rotation 
+         */
+        void setLocalRotation(const glm::quat& rotation);
+
+        /**
+         * @brief Updates this Transform's local rotation quaternion using angle-axis
+         * @param angle 
+         * @param axis 
+         */
+        void setLocalRotation(float angle, const glm::vec3& axis);
+
+        /**
+         * @brief Updates this Transform's local rotation quaternion using pitch, yaw, roll (XYZ Euler)
+         * @param pitch 
+         * @param yaw 
+         * @param roll 
+         */
+        void setLocalRotation(float pitch, float yaw, float roll);
+
+        // TODO: methods for setting global position and rotation. scale is kinda tricky...
+        
+
+
+    private:
+
+        // TODO: figure out how to flatten/linearize this nicely
+        Transform* parent = nullptr;
+
         // local transforms
-        glm::vec3 local_position{ 0.0f };
-        glm::vec3 local_scale{ 1.0f };
-        glm::quat local_rotation;
-        // global transforms
-        glm::mat4 global_transforms{ 1.0f };
-        bool dirty = false;
-    };
+        
+        glm::vec3 position{ 0.0f };
+        glm::vec3 scale{ 1.0f };
+        glm::quat rotation{ };
 
-    class TransformHierarchy
-    {
-        std::vector<Transform> transformPool;
+        // global transforms
+
+        glm::mat4 transformationMatrix{ 1.0f };
+        bool dirty = false;
     };
 }
